912c39c8a5685e3adb0674d042197cef4f0867e9
RoleyRoley <sonnyjohnjeffreys@gmail.com>
2026-03-02 18:37:36 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Sorting Algorithm Visualiser
+
+print("Which sortingalgorithm do you want to use?")
+print("1. Bubble Sort")
+print("2. Insertion Sort")
+print("3. Selection Sort")
+print("4. Merge Sort")
+print("5. Quick Sort")
+
+user_choice = input("Enter the number corresponding to your choice: ")
